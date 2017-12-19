1ca775eccf8852e06ff5e9260280e98022cefbc4
ajyadav013 <arjun.yadav013@gmail.com>
2017-12-19 12:10:52 +0530

    commented check default

--- app/bot.py
+++ app/bot.py
@@ -48,8 +48,8 @@ class Bot():
                 print('inside self.data after assigning channel')
                 print('Agent', self.agent)
                 #reply.send_typing(self.data) #Commented because it raise double response problem
-                #check_default = self.agent.handle_message(self.data['text'], None, OutputChannel(self.data))
-                #print('Check default', check_default)
+                check_default = self.agent.handle_message(self.data['text'], None, OutputChannel(self.data))
+                print('Check default', check_default)
                 # check_default = self.agent.handle_message(
                 #     self.data['text'], None,
                 #     OutputChannel(self.data))
